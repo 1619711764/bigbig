d2ab542bdf5603178ab5f2106a010e63c10b5d0c
lamb <1619711764@qq.com>
2023-05-11 16:21:14 +0800

    完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,26 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://big-event-api-t.itheima.net' + options.url
+
+  // 在发起请求头之前 为每个/my设置请求头
+  if(options.url.indexOf('/my/') !== -1){
+    options.headers = {
+      Authorization:localStorage.getItem('token') || ''
+    }
+  }
+
+  // 挂载全局complete对象
+  // 无论结果 都会complete回调
+  options.complete = function(res){
+    if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+      // 强制清空token
+      localStorage.removeItem('token')
+      // 跳转
+      location.href = './login.html'
+    }
+  
+  }
+
 })
+
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+  // 获取用户基本信息
+  getUserInfo();
+  const layer = layui.layer;
+
+  // 实现点击退出按钮跳转页面功能
+  $("#btnLogout").on("click", function () {
+    // console.log('rr');
+    // 提示用户
+    layer.confirm(
+      "确定退出登录?",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        localStorage.removeItem("token");
+        location.href = "./login.html";
+        layer.close(index);
+      }
+    );
+  });
+});
+// 获取用户信息
+function getUserInfo() {
+  $.ajax({
+    url: "/my/userinfo",
+    method: "GET",
+    // headers:{
+    //     Authorization:localStorage.getItem('token') || ''
+    // },
+    success: function (res) {
+      // console.log(res)
+      // 渲染用户头像
+      if (res.status !== 0) {
+        return layui.layer.msg("获取用户信息失败");
+      }
+      renderAvatar(res.data);
+    },
+  });
+}
+
+function renderAvatar(user) {
+  // 获取用户的名称
+  const name = user.nickname || user.username;
+  // 设置欢迎的文本
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+  // 获取用户头像
+  // 按需渲染用户头像
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    let first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+  }
+}
